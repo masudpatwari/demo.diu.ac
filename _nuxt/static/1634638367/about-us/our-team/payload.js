8f3cfd56c238829499b20a54d4a0a20c8099a298
OmorFaruk00 <omorfaruk5020@gmail.com>
2023-10-05 17:53:11 +0600

    project upload

---
+++ _nuxt/static/1634638367/about-us/our-team/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/about-us/our-team", {data:[{}],fetch:{},mutations:[]});
---
+++ data/socialMedias.js
@@ -0,0 +1,33 @@
+export const socialMedias = [
+    {
+        title: 'Facebook',
+        icon:'icon-facebook',
+        url:'https://www.facebook.com/TCRCDIU/',
+    },
+  {
+    title: 'Twitter',
+    icon:'icon-twitter',
+    url:'https://twitter.com/diubd',
+  },
+    {
+        title: 'Youtube',
+        icon:'icon-youtube',
+        url:'https://www.youtube.com/diudhaka',
+    },
+
+    {
+        title: 'Linkedin',
+        icon:'icon-linkedin',
+        url:'https://www.linkedin.com/school/dhaka-international-university',
+    },
+   /* {
+        title: 'Instagram',
+        icon:'icon-instagram',
+        url:'https://www.instagram.com/diu.net.bd',
+    },
+    {
+        title: 'Pinterest',
+        icon:'icon-pinterest',
+        url:'https://www.pinterest.com/DIUdhaka',
+    },*/
+];
